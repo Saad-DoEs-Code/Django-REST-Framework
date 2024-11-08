a2909cea454ece9f29dc5a540bb9642eac4d103c
Saad Does Code <saad.does.code@gmail.com>
2024-11-08 22:52:42 +0500

    Initial Commit

---
+++ py_client/basic.py
@@ -0,0 +1,11 @@
+import requests
+
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://127.0.0.1:8000/api/"
+
+get_response = requests.get(endpoint)
+print(get_response) # Returns Status Code
+print(get_response.text) # Returns Text/Raw Text of HTML Page Structure
+
+print(get_response.json()) # Returns the JSON Data in the form of Python Object
+print(get_response.json()['message']) # Returns the Value against the specified Key
