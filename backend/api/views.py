c7cc4152aa6114f922acf088b6500d628b27f0c8
Saad Does Code <saad.does.code@gmail.com>
2024-11-09 01:38:12 +0500

    ECHO GET JSON

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,22 @@
 from django.http import JsonResponse
+import json
 
 def api_home(request, *args, **kwargs):
 
-    return JsonResponse({"message":"Hello Saad ur Rehman"})
+    # print(request.GET) # Returns URL params
+    body = request.body
+    data = {}
+    # This Try/Except Block is necessary as it can get an empty JSON Data
+    try:
+        data = json.loads(body) # Converts JSON Data into PYTHON Dictionary
+    except:
+        pass
+
+    # We can add DATA to our "data" variable
+    data['headers'] = dict(request.headers)
+    data['method'] = request.method
+    data['content_type'] = request.content_type
+    data['params'] = dict(request.GET)    
+    # print(data.keys())
+
+    return JsonResponse(data)
